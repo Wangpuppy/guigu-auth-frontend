5ae81bf889b1026c4bdcf8bdc012feed56428fe2
Ruiyang Wang <895437987@qq.com>
2023-10-11 14:19:16 +0800

    feat: 角色列表-角色添加

--- src/api/role/role.js
+++ src/api/role/role.js
@@ -4,24 +4,33 @@ import request from '@/utils/request'
 //常量
 const api_name = '/admin/system/sysRole/'
 export default {
-    //列表
-    getPageList(page,limit,searchObj){
-        return request({
-            //接口路径
-            url: `${api_name}/${page}/${limit}`,
-            method: 'get', //提交方式
-            //参数
-            params: searchObj
-        })
-    },
-    //删除
-    removeId(id){
-        return request({
-            //接口路径
-            url: `${api_name}/remove/${id}`,
-            method: 'delete', //提交方式
-        })
-    },
-
+  //列表
+  getPageList(page, limit, searchObj) {
+    return request({
+      //接口路径
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get', //提交方式
+      //参数
+      params: searchObj
+    })
+  },
+  //删除
+  removeId(id) {
+    return request({
+      //接口路径
+      url: `${api_name}/remove/${id}`,
+      method: 'delete', //提交方式
+    })
+  },
+  //添加
+  save(role) {
+    return request({
+      //接口路径
+      url: `${api_name}/save`,
+      method: 'post', //提交方式
+      //通过json形式传递数据，传递json格式
+      data: role
+    })
+  },
 }
 
--- src/views/system/sysRole/list.vue
+++ src/views/system/sysRole/list.vue
@@ -12,48 +12,71 @@
           </el-col>
         </el-row>
         <el-row style="display:flex">
-          <el-button type="primary" icon="el-icon-search" size="mini"  @click="fetchData()">搜索</el-button>
+          <el-button type="primary" icon="el-icon-search" size="mini" @click="fetchData()">搜索</el-button>
           <el-button icon="el-icon-refresh" size="mini" @click="resetData">重置</el-button>
         </el-row>
       </el-form>
     </div>
+
+    <!-- 工具条 -->
+    <div class="tools-div">
+      <el-button type="success" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>
+    </div>
+
     <!-- 表格 -->
     <el-table
-        v-loading="listLoading"
-        :data="list"
-        stripe
-        border
-        style="width: 100%;margin-top: 10px;">
+      v-loading="listLoading"
+      :data="list"
+      stripe
+      border
+      style="width: 100%;margin-top: 10px;">
 
       <el-table-column
-          label="序号"
-          width="70"
-          align="center">
+        label="序号"
+        width="70"
+        align="center">
         <template slot-scope="scope">
           {{ (page - 1) * limit + scope.$index + 1 }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="roleName" label="角色名称" />
-      <el-table-column prop="roleCode" label="角色编码" />
+      <el-table-column prop="roleName" label="角色名称"/>
+      <el-table-column prop="roleCode" label="角色编码"/>
       <el-table-column prop="createTime" label="创建时间" width="160"/>
       <el-table-column label="操作" width="200" align="center">
         <template slot-scope="scope">
           <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(scope.row.id)" title="修改"/>
-          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeDataById(scope.row.id)" title="删除"/>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeDataById(scope.row.id)"
+                     title="删除"/>
         </template>
       </el-table-column>
     </el-table>
 
     <!-- 分页组件 -->
     <el-pagination
-        :current-page="page"
-        :total="total"
-        :page-size="limit"
-        style="padding: 30px 0; text-align: center;"
-        layout="total, prev, pager, next, jumper"
-        @current-change="fetchData"
+      :current-page="page"
+      :total="total"
+      :page-size="limit"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="fetchData"
     />
+
+    <!--  弹框  -->
+    <el-dialog title="添加/修改" :visible.sync="dialogVisible" width="40%">
+      <el-form ref="dataForm" :model="sysRole" label-width="150px" size="small" style="padding-right: 40px;">
+        <el-form-item label="角色名称">
+          <el-input v-model="sysRole.roleName"/>
+        </el-form-item>
+        <el-form-item label="角色编码">
+          <el-input v-model="sysRole.roleCode"/>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false" size="small" icon="el-icon-refresh-right">取 消</el-button>
+        <el-button type="primary" icon="el-icon-check" @click="saveOrUpdate()" size="small">确 定</el-button>
+      </span>
+    </el-dialog>
 
   </div>
 </template>
@@ -65,14 +88,18 @@ import th from "element-ui/src/locale/lang/th";
 
 export default {
   //定义初始值
-  data(){
-    return{
-      listLoading:true,//是否显示加载
-      list:[],//角色列表
-      total:0,//总记录数
-      page:1,//当前页
-      limit:3,//每页显示记录数
-      searchObj:{}//条件查询封装对象
+  data() {
+    return {
+      listLoading: true,//是否显示加载
+      list: [],//角色列表
+      total: 0,//总记录数
+      page: 1,//当前页
+      limit: 3,//每页显示记录数
+      searchObj: {},//条件查询封装对象
+
+      dialogVisible: false,//是否显示弹出框
+      sysRole: {}//封装添加表单的数据
+
     }
   },
   //页面渲染之前执行
@@ -81,8 +108,45 @@ export default {
     this.fetchData()
   },
   methods: {//具体方法
+    //点击确定
+    saveOrUpdate() {
+      //判断添加还是修改
+      if (!this.sysRole.id){
+        this.saveRole()
+      }else {
+        this.updateRole()
+      }
+
+    },
+    //修改的方法
+    updateRole(){
+
+    },
+    //添加的方法
+    saveRole() {
+      api.save(this.sysRole)
+        .then(response => {
+          //提示
+          this.$message({
+            type: 'success',
+            message: '添加成功!'
+          });
+          //关闭弹框
+          this.dialogVisible = false
+          //刷新页面
+          this.fetchData()
+        })
+    },
+
+    //点击添加，弹出框
+    add() {
+      this.dialogVisible = true
+      this.sysRole = {}
+
+    },
+
     //删除
-    removeDataById(id){
+    removeDataById(id) {
       this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -90,20 +154,20 @@ export default {
       }).then(() => {
         //调用方法删除
         api.removeId(id)
-            .then(response=>{
-              //提示
-              this.$message({
-                type: 'success',
-                message: '删除成功!'
-              });
-              //刷新页面
-              this.fetchData()
-            })
+          .then(response => {
+            //提示
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+            //刷新页面
+            this.fetchData()
+          })
 
       })
     },
     //重置
-    resetData(){
+    resetData() {
       //清空表单
       this.searchObj = {}
 
@@ -113,25 +177,23 @@ export default {
 
     //条件分页查询列表
     //pageNum 查询页数 默认为1
-    fetchData(pageNum=1) {
+    fetchData(pageNum = 1) {
       //页数赋值
-      this.page =  pageNum
+      this.page = pageNum
       //ajax
-      api.getPageList(this.page,this.limit,this.searchObj)
-          .then(response =>{
-            this.listLoading = false
-            // console.log(response)
-            //每页数据列表
-            this.list = response.data.records
-            //总记录数
-            this.total = response.data.total
-          })
+      api.getPageList(this.page, this.limit, this.searchObj)
+        .then(response => {
+          this.listLoading = false
+          // console.log(response)
+          //每页数据列表
+          this.list = response.data.records
+          //总记录数
+          this.total = response.data.total
+        })
 
     }
 
   }
-
-
 
 
 }
